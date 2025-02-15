022c6fbe108d36fc3f5004dae4bcfee7fdd41cfa
İlayda Giriş <101815936+ilaydagiris@users.noreply.github.com>
2025-02-15 17:48:22 +0200

    Time: 0 ms (100%), Space: 42 MB (65.35%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,16 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-                int k = 0;
-                
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
-            }
+    int k = 0;
+    for (int num : nums) {
+        if (num != val) {
+            nums[k++] = num;
         }
-
-        System.out.println(Arrays.toString(Arrays.copyOf(nums, k)));
-        
-        return k;
+    }
+    return k;
     }
 }
